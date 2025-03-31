2787129fdfa75cd5330f81b6b31802be35a89b6b
heosangbeom <luv_3_u@naver.com>
2025-04-01 08:10:09 +0900

    fix: update package configuration for pub.dev

--- test/info_ui_package_test.dart
+++ test/info_ui_package_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:info_ui_package/info_ui_package.dart';
+import 'package:flutter_list_ui/flutter_list_ui.dart';
 
 void main() {
   test('adds one to input values', () {
-    // Add your tests here
+    // TODO: Implement test
   });
 }
